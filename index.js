01ea412be6428fd703748bc96205e26ec8458a42
FEZ (Felix Kratzer) <felix@kratzer.org>
2022-01-01 15:14:43 +0000

    Initial checkin for creating image

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const fs = require('fs')
+const p5 = require('node-p5');
 
-const content = '<html><body>Hello World' + (new Date()) + '</body></html>'
 
-fs.writeFile('build/index.html', content, err => {
-    if (err) {
-        console.error(err)
-        return
+
+const drawFlower = (p) => {
+    p.background(
+        p5.random(50, 255),
+        p5.random(50, 255),
+        p5.random(50, 255)
+    );
+    p.text('hello world!', 50, 100);
+}
+
+function sketch(p) {
+    p.setup = () => {
+        let canvas = p.createCanvas(1024, 1024);
+
+        drawFlower(p);
+
+        p.saveCanvas(canvas, 'build/flower', 'jpg').then(filename => {
+            console.log(`saved the canvas as ${filename}`);
+        });
+        p.noLoop();
     }
-    //file written successfully
-})
+
+}
+
+console.log("Start")
+let p5Instance = p5.createSketch(sketch);
